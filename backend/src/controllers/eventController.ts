413f650684ce9c9e0749bf2b22571c444e539846
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2023-05-23 18:25:55 +0100

    tidy controller

--- backend/src/controllers/eventController.ts
+++ backend/src/controllers/eventController.ts
@@ -1,9 +1,8 @@
 import { Request, Response } from "express";
-// import asyncHandler from "express-async-handler";
-import Event from "../models/event"; // Assuming you have a model called Event
+import Event from "../models/event";
 
 const eventController = {
-  findAll: async (req: Request, res: Response) => {
+  findAll: async (req: Request, res: Response): Promise<Response> => {
     const page = parseInt(req.query.page as string) || 1; // Current page number
     const limit = parseInt(req.query.limit as string) || 10; // Number of items per page
 
@@ -12,10 +11,12 @@ const eventController = {
 
     const queryResult = await Event.findAndCountAll({
       where: { care_recipient_id: req.params.care_recipient_id },
+      order: [["timestamp", "DESC"]],
       limit,
       offset,
     });
 
+    // this needs to sort by visit and catalogue that by date for the sake of the graph
     const events = queryResult.rows;
     const totalItems = queryResult.count;
     const totalPages = Math.ceil(totalItems / limit);
