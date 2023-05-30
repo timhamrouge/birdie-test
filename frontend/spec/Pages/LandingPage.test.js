c816adbeea688e01caa06300ac205dbebe63d0d7
timhamrouge <34589526+timhamrouge@users.noreply.github.com>
2023-05-30 18:02:57 +0100

    starting front end tests

---
+++ frontend/spec/Pages/LandingPage.test.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import LandingPage from '../../src/components/Pages/LandingPage'
+
+test('renders learn react link', () => {
+  render(<LandingPage />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
